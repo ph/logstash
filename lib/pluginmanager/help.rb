1777d96e10a8e0d8881f43ce8a8bf8a7e06e6abd
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-06-19 11:55:37 -0400

    adding help command

---
+++ lib/pluginmanager/help.rb
@@ -0,0 +1,25 @@
+require "lib/logstash/docgen/parser"
+
+class LogStash::PluginManager::Help < LogStash::PluginManager::Command
+  parameter "[PLUGIN]", "Display help for this plugin"
+
+  def execute
+    LogStash::Bundler.setup!({:without => [:build, :development]})
+
+    signal_usage_error("You have to specify a plugin") unless plugin
+    signal_usage_error("This plugin doesn't exist") unless plugin_paths
+
+    puts plugin_paths.inspect
+    context = LogStash::Docgen::Parser.parse(plugin_paths.first)
+    help = LogStash::Docgen::HelpFormat.new()
+    help.generate(context)
+  end
+
+  def plugin_paths
+    @plugin_paths =  begin
+                      gemspecs = LogStash::PluginManager.find_plugins_gem_specs(plugin)
+                      return [] if gemspecs.empty?
+                      Dir.glob(File.join(gemspecs.first.gem_dir, "lib", "logstash", "{input,output,filter,codec}s", "*.rb"))
+                    end
+  end
+end
