f22e4b9c68381ad393cdc49592560176cda12b8d
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-06-19 11:55:19 -0400

    wip

--- lib/logstash/docgen/parser.rb
+++ lib/logstash/docgen/parser.rb
@@ -1,6 +1,6 @@
 module LogStash::Docgen
   class PluginContext
-    attr_accessor :description, :config_name
+    attr_accessor :description, :config_name, :section, :class_name
   
     def initialize
       @config = Hash.new({})
@@ -16,18 +16,32 @@ module LogStash::Docgen
   end
 
   class DynamicParser
+    def initialize(file, context)
+      @file = file
+      @context = context
+    end
+
+    def parse
+      load @file
+      klass
+    end
+
+    def klass
+      @context.class_name.split('::').inject(Object) { |memo,name| memo = memo.const_get(name); memo }
+    end
   end
 
   # This class only do the static parsing
   # options and comments
   class Parser
     COMMENT_RE = /^ *#(?: (.*)| *$)/
+    ENDLINES_RE = /\r\n|\n/
     COMMENTS_IGNORE = ["encoding: utf-8"]
 
     def initialize
       @rules = {
         COMMENT_RE => lambda { |m| parse_comment(m[1]) },
-        /^ *class.*< *(::)?LogStash::(Outputs|Filters|Inputs|Codecs)::(Base|Threadable)/ => lambda { |m| parse_class_description },
+        /^ *class\s(.*) < *(::)?LogStash::(Outputs|Filters|Inputs|Codecs)::(Base|Threadable)/ => lambda { |m| parse_class_description(m) },
         /^ *config +[^=].*/ => lambda { |m| parse_config(m[0]) },
         /^ *config_name .*/ => lambda { |m| parse_config_name(m[0]) },
         /^ *(class|def|module) / => lambda { |m| reset_buffer },
@@ -38,7 +52,9 @@ module LogStash::Docgen
       reset_buffer
     end
 
-    def parse_class_description
+    def parse_class_description(class_definition)
+      @context.section = class_definition[3].downcase.gsub(/s$/, '')
+      @context.class_name = class_definition[1]
       @context.description = flush_buffer
     end
 
@@ -59,12 +75,16 @@ module LogStash::Docgen
     def parse(file)
       string = File.read(file)
       extract_lines(string).each { |line| parse_line(line) }
+
+      dynamic = DynamicParser.new(file, @context)
+      dynamic.parse
+
       return @context
     end
 
     def extract_lines(string)
       buffer = ""
-      string.split(/\r\n|\n/).collect do |line|
+      string.split(ENDLINES_RE).collect do |line|
         # Join long lines
         if line =~ COMMENT_RE
           # nothing
@@ -114,6 +134,8 @@ module LogStash::Docgen
   # doc representation
   class HelpFormat
     def generate(context)
+      puts context.section + " " + context.class_name
+      puts "\n\n"
       puts context.config_name
       puts "\n\n"
       puts context.description
